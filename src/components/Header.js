55d6c6e3acb88da46ebccbcd2df873f120231aee
Samuel Oyedeji <oyedeji.smt@gmail.com>
2023-01-13 17:58:21 +0100

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Search from './Search'
+
+function Header() {
+  return (
+    <div>
+        <h1>recipclopedia</h1>
+        <Search />
+    </div>
+  )
+}
+
+export default Header
